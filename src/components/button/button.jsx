e695118c45c692c9c1740f8dfbe72e33b003112f
Zeverouis <Zeverouis@gmail.com>
2025-08-17 23:52:16 +0200

    adjusted button.jsx, button.css. Added dynamic background images to buttons

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,18 +1,27 @@
 import './button.css'
 import {Link} from 'react-router-dom';
 
-function Button({label, to, onClick, variant = 'primary'}) {
-    const classNames = ['button', `button-${variant}`].join(' ');
+function Button({label, to, onClick, variant = 'primary', backgroundImage = null}) {
+    const classNames = ['button', `button-${variant}`];
+
+    if (backgroundImage) {
+        classNames.push('button-background-image');
+    }
+
+    const className = classNames.join(' ');
+
+    const style = backgroundImage ? {'--background-image-url' : `url(${backgroundImage})`} : undefined;
+
     if (to) {
     return (
-        <Link to={to} className={classNames} onClick={onClick}>
+        <Link to={to} className={className} onClick={onClick} style={style}>
             {label}
         </Link>
     );
 }
 
 return (
-    <button className={classNames} onClick={onClick}>
+    <button className={className} onClick={onClick} style={style}>
         {label}
     </button>
 );
