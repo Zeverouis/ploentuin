d78450af83e0821038274ecf70ff9cd9910cd358
Zeverouis <Zeverouis@gmail.com>
2026-02-25 21:52:19 +0100

    Added assets (icons/logo)

---
+++ src/assets/database/categories/category-images.jsx
@@ -0,0 +1,20 @@
+export const categoryImages = {
+    //Tuin Tips
+    1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJkcZIzHKSPjbJV5jwzJZTDRtbYCbz3ekjEg&s",
+    //Groenten
+    2: "https://midmid.blob.core.windows.net/images/filip-peggy/collection/main/groenten_21c8eb_e23c40_sm.jpg?v=b853439241c2607e8873bdc6310c5f0e",
+    //Bloemen
+    3: "https://static.hanos.nl/medias/34902990.jpg-256Wx256H?context=bWFzdGVyfHByb2R1Y3RpbWFnZXN8NTU0NjR8aW1hZ2UvanBlZ3xjM2x6TFcxaGMzUmxjaTl3Y205a2RXTjBhVzFoWjJWekwyZ3dOeTlvWlRBdk9UYzNOVGM1TkRnNE1EVTBNaTh6TkRrd01qazVNQzVxY0dkZk1qVTJWM2d5TlRaSXwwYjZmYjkwNzFjNWYwNzRkNzgzYmFlZGEzZTE1NzY4NTM1YzE0NWJlZGE5YjY2NDdmYjY2MGUyMjk3OWJmMjA1",
+    //Struiken
+    4: "https://static.ufurnish.com/assets%2Fproduct-images%2Fb-and-q%2Fgbbaqbaq1u5z0gupuz6ee%2Fthompson-morgan-box-hedging-collection-10-potted-plants_thumb-209357398.jpg",
+    //Bomen
+    5: "https://images.stockcake.com/public/9/a/e/9aeaa7cd-50e9-4b29-bd06-a389d8294375_medium/ancient-forest-magic-stockcake.jpg",
+    //Heesters
+    6: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbLakTzSRiZqdSXJ6uSAobffmhKW5nC767Qw&s",
+    //Kruiden & Specerijen
+    7: "https://cdn.maxvandaag.nl/2024/10/kruiden-en-specerijen-shutterstock-900x500-1-890x500.jpg",
+    //Waterplanten
+    8: "https://images.stockcake.com/public/4/a/e/4ae043e2-224d-458d-ab31-377daf4b52b5_medium/aquatic-life-flourishing-stockcake.jpg",
+    //Fruit
+    9: "https://images.stockcake.com/public/0/2/2/0224e2cf-ae0a-4fa8-9406-4f7e08151d52_medium/colorful-fruit-stand-stockcake.jpg"
+};
